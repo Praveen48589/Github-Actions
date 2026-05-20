8bc6c081d4fa0bc0ff6dd683570a7cc5b8933484
Praveen48589 <tomarpraveen640@gmail.com>
2026-05-20 14:31:58 +0530

    docker build-push

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def hello_world():
 def health():
     return 'Server is up and running'
 
-app.run(debug=False, host='0.0.0.0', port=80, threaded=True)
+app.run(debug=True, host='0.0.0.0', port=80)
